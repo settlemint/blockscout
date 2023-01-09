d43bd09a19b12f96ae1d61787881d88c7b61d3eb
Patrick Mualaba <patrick.mualaba@live.be>
2023-01-09 15:28:18 +0100

    debug ws paths

--- apps/block_scout_web/lib/block_scout_web/router.ex
+++ apps/block_scout_web/lib/block_scout_web/router.ex
@@ -25,7 +25,7 @@ defmodule BlockScoutWeb.Router do
     plug(:accepts, ["json"])
   end
 
-  forward("/api", ApiRouter)
+  forward("/insights/#{System.get_env("postgres_db", nil)}/api", ApiRouter)
 
   if Application.compile_env(:block_scout_web, ApiRouter)[:reading_enabled] do
     # Needs to be 200 to support the schema introspection for graphiql
