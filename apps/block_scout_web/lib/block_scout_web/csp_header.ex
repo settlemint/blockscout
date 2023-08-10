20f92ad73e20851b6c5f92813723b3c45c6e21a7
Patrick Mualaba <patrick.mualaba@live.be>
2023-08-10 10:57:24 +0200

    fix csp headers

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -26,8 +26,8 @@ defmodule BlockScoutWeb.CSPHeader do
         img-src 'self' * data:;\
         media-src 'self' * data:;\
         font-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.gstatic.com data:;\
-        frame-src 'self' 'unsafe-inline' 'unsafe-eval' #{czilladx_url} #{google_url};\
-        frame-ancestors http://localhost:3002/ http://onprem.settlemint.local/;\
+        frame-src 'self' http://onprem.settlemint.local/ 'unsafe-inline' 'unsafe-eval' #{czilladx_url} #{google_url};\
+        frame-ancestors 'self' http://localhost:3002/ http://onprem.settlemint.local/;\
         manifest-src 'self' https://*.settlemint.com/ http://onprem.settlemint.local/;\
       "
     })
